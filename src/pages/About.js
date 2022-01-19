42ada451f433c8fe9bcbc69d232e8c5662744103
cyxl <cyxlpublic@gmail.com>
2022-01-19 15:17:44 -0700

    Improving About me

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,28 @@
 import React from "react";
-import '../css/About.css'
+import "../css/About.css";
 
 function About() {
   return (
-      <h1>About</h1>
+    <div className="About">
+      <div className="left-panel">
+        <p></p>
+      </div>
+
+      <div className="right-panel">
+        <div className="content">
+          <p className="text-header">
+            I am a Self Taught Developer from the United States
+          </p>
+          <p className="text-description">
+            Developing has been a passion of mine for a while now.
+            <br /> When I first started coding it was rough lots of new stuff.
+            <br />I just keep on learning more and having fun!
+          </p>
+          <h3 className="developing-header">Developing</h3>
+          <p className="text-description"></p>
+        </div>
+      </div>
+    </div>
   );
 }
 
